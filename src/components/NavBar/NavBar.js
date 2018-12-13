edd9aef786f3a24e0bd5dca5ff9034ae5c45ad07
Crystal C <crystal.contr@gmail.com>
2018-12-12 23:50:47 -0600

    Removed unnecessary spaces and lines.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-const links = ['Home', 'About', 'Portfolio', 'Rates', 'Contact'];
-
 class NavBar extends React.Component{
     constructor(props) {
         super(props);
       
-        this.sortByOptions = ['Contact', 'Rates', 'Portfolio', 'About', 'Home'];
+        this.links = ['Contact', 'Rates', 'Portfolio', 'About', 'Home'];
         this.listItems = this.listItems.bind(this);
     }
 
     listItems() {
-        return this.sortByOptions.map(link => {
+        return this.links.map(link => {
             let hrefLink = `./${link}/${link}.html`;
             return (<li className="nav-item">
                         <a className="Header-nav-item" href={hrefLink}>
                             {link}
                         </a>
-                    </li>);     
+                    </li>
+            );     
         });
     }
 
